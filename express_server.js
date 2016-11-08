cb5e4e868dc8d54564f64410e55972ce00c0d5c8
Behzad Azampour <behy1368@gmail.com>
2016-11-08 21:34:36 +0000

    committing working updates up to and including Template Engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,6 +16,23 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+
+
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  console.log(urlDatabase[req.params.id]);
+  res.render("urls_show", templateVars);
+});
+
+
+
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b> </body> </html> \n");
